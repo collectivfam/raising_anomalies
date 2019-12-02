d2f7e1899fec1ea6153360f214306ea4ff59a879
techtolentino <makertolentino@gmail.com>
2019-12-02 01:54:30 -0600

    migrate updates to site

---
+++ src/constants/updates-data.js
@@ -0,0 +1,160 @@
+export default [
+	{
+		date: "Monday, December 2, 2019",
+		motivation: "•••••",
+		notes: [
+			"Migrated daily updates to site",
+			"Made decision to return Peter's equipment",
+			"New goal: a chapter per week, NMW (no matter what)",
+		]
+	},
+	{
+		date: "Sunday, December 1, 2019",
+		motivation: "•••••",
+		notes: [
+			"Purchased my first Author's Chair (and ottomon)",
+			"Continued Better Humans article",
+			"Visited bookstore and envisioned my book under the Parenting section",
+		]
+	},
+	{
+		date: "Saturday, November 30, 2019",
+		motivation: "•••••",
+		notes: [
+			"Progress with Better Humans article",
+			"Read Introduction of Raising Anomalies to my wife out loud",
+			"Decided to convert home studio to a Writers studio (rearranging physical space and intent)",
+			"Added Google analytics"
+		]
+	},
+	{
+		date: "Friday, November 29, 2019",
+		motivation: "•••••",
+		notes: [
+			"Continued writing Better Humans article",
+			"Missed sync with Shaan, will catch up next week",
+			"Deployed site to netlify with custom domain"
+		]
+	},
+	{
+		date: "Thursday, November 28, 2019",
+		motivation: "•••••",
+		notes: [
+			"Thanksgiving, no writing",
+			"Still interviewing Dad, taking mental notes again",
+		]
+	},
+	{
+		date: "Wednesday, November 27, 2019",
+		motivation: "•••••",
+		notes: [
+			"Writing break (father was in town)",
+			"With my Dad in town, I was in mental-note-taking mode",
+			"Told my Dad I was writing a book",
+		]
+	},
+	{
+		date: "Tuesday, November 26, 2019",
+		motivation: "•••••",
+		notes: [
+			"Read article on Self-Publishing myths",
+			"Paused Chapter 1 (just for Thanksgiving break)",
+			"Began writing Better Humans article (separate work, but still writing)"
+		]
+	},
+	{
+		date: "Monday, November 25, 2019",
+		motivation: "•••••",
+		notes: [
+			"Finished drafting Introduction",
+			"Began Chapter 1, A Small Life",
+		]
+	},
+	{
+		date: "Sunday, November 24, 2019",
+		motivation: "•••••",
+		notes: [
+			"Added FAQ data, rendered in author page",
+			"removing updates section on homepage, and adding page"
+		]
+	},
+	{
+		date: "Saturday, November 23, 2019",
+		motivation: "••••",
+		notes: [
+			"Created book mock for the website (all placeholder)",
+			"Redid navigation",
+			"Worked on hero",
+		]
+	},
+	{
+		date: "Friday, November 22, 2019",
+		motivation: "••••",
+		notes: [
+			"Worked on the site's hero section",
+		]
+	},
+	{
+		date: "Thursday, November 21, 2019",
+		motivation: "•••••",
+		notes: [
+			"Continued Introduction, Section II (small update)",
+			"Stepped outside of the Introduction work to outline a separate article for Better Humans",
+		]
+	},
+	{
+		date: "Wednesday, November 20, 2019",
+		motivation: "•••••",
+		notes: [
+			"Continued Introduction, Section II (maybe section III?)",
+			"Researching patterns in content organization, looking at Garrison Keillor's The Keillor Reader for now",
+			"Received answers from Bobby's Dad, marinating on them",
+			"Slight update to the website (updated with another draft subheading)",
+			"Tweaked the mint design (in Figma) for what might represent an anomaly"
+		]
+	},
+	{
+		date: "Tuesday, November 19, 2019",
+		motivation: "•••••",
+		notes: [
+			"Continued Introduction, Section II",
+			"prepared questions for Bobby's dad (self-published author)",
+		]
+	},
+	{
+		date: "Monday, November 18, 2019",
+		motivation: "•••••",
+		notes: [
+			"slight edits to Introduction chapter, first section",
+			"researched IngramSpark vs KDP print on demand options",
+			"edited Introduction, Section I, while at Pax's RE class",
+			"discussed book with Eric, Hyun, Josh, and Bobby"
+		]
+	},
+	{
+		date: "Sunday, November 17, 2019",
+		motivation: "•••••",
+		notes: [
+			"converted outline to Google Drive structure",
+			"designed 5th potential cover",
+			"began Introduction chapter",
+			"researched publishing methods"
+		]
+	},
+	{
+		date: "Saturday, November 16, 2019",
+		motivation: "•••••",
+		notes: [
+			"opened a repository for site, progress tracking",
+			"researched Wise Ink, an indie author book publisher",
+			"began an outline of the book (potential table of contents)"
+		]
+	},
+	{
+		date: "Friday, November 15, 2019",
+		motivation: "•••••",
+		notes: [
+			"accountability partner established, Shaan Shah",
+		]
+	}
+]
--- src/templates/updates.js
+++ src/templates/updates.js
@@ -2,12 +2,30 @@ import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/SEO"
 
+import updatesData from "../constants/updates-data"
+
 export default ({ pageContext: { page } }) => (
   <Layout>
     <Seo title={page.name} description={page.title} />
     <div className="page-standard">
       <h1>updates</h1>
-      <p>coming soon...</p>
+      <section className="page-section">
+      {updatesData.map((item, i) => (
+        <div className="update" key={i}>
+          <span className="update-date">{item.date}</span>
+          
+          Motivation: <span className="update-rating">{item.motivation}</span>
+          
+          <ul className="update-notes">
+          {item.notes.map((line, i) => (
+            <li className="update-notes-item" key={i}>{line}</li>
+          ))}
+          </ul>
+          
+        </div>
+      )
+      )}
+      </section>
     </div>
   </Layout>
 )
